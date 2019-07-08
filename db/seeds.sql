a286d2a59c30819cab03f140a4d251697f6c61dc
Michael Barachkov <mike.barachkov@outlook.com>
2019-07-08 13:51:18 -0700

    linking orm

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic American', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bleu Cheese Mushroom', false);
