04aead915b2cd71dea3d989d881e02f235d0f37b
Michael Barachkov <mike.barachkov@outlook.com>
2019-07-08 21:56:20 -0700

    added the model

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,23 @@
 var orm = require("../config/orm.js");
 
-var burger = {}
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res){
+            cb(res);
+        })
+    },
+    create: function(name, cb) {
+        orm.create("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, coindition, cb)
+    }
+}
+module.exports = burger;
